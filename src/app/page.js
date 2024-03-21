77e75092498668174d6b7788d7fb7e9d57cdd128
theojames222 <james.theo222@gmail.com>
2024-03-20 21:55:15 -0400

    add home header component

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
+import HomeHeader from "@/components/HomeHeader";
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <HomeHeader />
+    </>
+  );
 }
